4473c5b261ef0fe44f48c8a4ba4926b1e87047f6
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-09-30 03:20:34 -0300

    Configurando o ejs

--- indroducao/src/app.js
+++ indroducao/src/app.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
+const views = path.join(__dirname, '/views')
+
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
   res.send('Olá mundo')
+})
+
+app.get('/nome/:nome', (req, res) => {
+  res.send(`Nome: ${req.params.nome}`)
+})
+
+app.get('/tecnologia', (req, res) => {
+  res.render(views + '/secao/tecnologia')
 })
 
 app.listen(3000, () => {
